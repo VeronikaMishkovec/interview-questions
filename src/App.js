42d24b41b282cc3d42e52d81dd0e252ac75ff131
VeronikaMishkovec <veronika.kireenko@gmail.com>
2021-09-17 12:57:14 +0300

    react native navigation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { Collapse } from 'antd'
 import { data } from './data'
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'
+import { Layout } from 'antd'
+import './styles.scss'
 
 export const App = () => {
   const { Panel } = Collapse
+  const { Header, Footer, Sider, Content } = Layout
+
+  // const text = (text) => return {}
 
   const renderQuestions = data.map((item) => {
     return (
-      <Panel header={item.title} key={item.id}>
-        <p>{item.text}</p>
+      <Panel header={`${item.id}.  ${item.title}`} key={item.id}>
+        <div dangerouslySetInnerHTML={{ __html: item.text }} />
       </Panel>
     )
   })
 
-  return <Collapse defaultActiveKey={['1']}>{renderQuestions}</Collapse>
+  return (
+    <Layout>
+      <Header className={'header'}>
+        Вопросы к собеседованию React и React Native
+      </Header>
+      <Content>
+        <Collapse>{renderQuestions}</Collapse>
+      </Content>
+      {/* <Footer>Footer</Footer> */}
+    </Layout>
+  )
 }
 
 export default App
